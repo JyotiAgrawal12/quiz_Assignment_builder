2f1fc40e75e81b8862113c72c3319da04b57f1e1
JyotiAgrawal12 <jyoti.agrawal1631@gmail.com>
2022-05-26 20:25:26 +0530

    final commit

--- QuizzApp-main/src/components/Header.js
+++ QuizzApp-main/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div className="header">
-            <h1>Quiz Builder App</h1>
+            <h1><center>Quiz Builder App</center></h1>
         </div>
     )
 }
